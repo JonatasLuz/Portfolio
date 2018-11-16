7bebc82b220796fc4f01644cbd15331920fa24e2
Jonatas da Luz <jonatas.luz.91@gmail.com>
2018-11-16 11:05:50 -0200

    Criado Skills atraves de model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@posts = Blog.all #Instancia todos os posts Blog na variavel posts
+  	@skills = Skill.all #Instancia todas as skills Skill na variavel skills
   end
 
   def about
