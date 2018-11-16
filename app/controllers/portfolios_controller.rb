ee5ee1261af3fa457afa162aeea78699db67b77a
Jonatas da Luz <jonatas.luz.91@gmail.com>
2018-11-16 15:45:59 -0200

    Criado ação para criação de um item portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,20 @@
 class PortfoliosController < ApplicationController
+	def index
+		@portfolios_items = Portfolio.all
+	end
+	def new
+		@portfolio_item = Portfolio.new
+	end
+	def create
+  		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
+    	respond_to do |format|
+      		if @portfolio_item.save
+        		format.html { redirect_to portfolios_path, notice: 'Portfolio criado com sucesso.' }
+        		format.json { render :show, status: :created, location: @portfolio }
+      		else
+        		format.html { render :new }
+        		format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      		end
+    	end
+ 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,34 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+	Blog.create(
+		title:"My blog #{blog}",
+		body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		Suspendisse vel leo mi. Integer eget ullamcorper nibh. Maecenas nec nibh condimentum, aliquet tellus vel, auctor leo. 
+		Curabitur libero metus, ornare ac urna et, fermentum fermentum mauris. 
+		Nunc commodo vulputate est, non luctus leo imperdiet condimentum. 
+		Pellentesque tincidunt nibh sit amet elit scelerisque ultricies. Pellentesque vitae vulputate arcu."
+	)
+end
+
+5.times do |skill|
+	Skill.create(
+		title:"Minha skill #{skill}",
+		percent_utilized:"5"
+	)
+end
+
+9.times do |portfolio|
+	Portfolio.create(
+		title:"Meu portfolio #{portfolio}",
+		subtitle:"#{portfolio} portfolio",
+		body:"Nullam eu mattis nibh. Nulla lorem augue, venenatis sed lacus eget, pharetra tristique erat. 
+		Iteger sit amet lectus risus. Ut id neque a tellus pellentesque commodo. In et vehicula nisi. 
+		Praesent eget imperdiet ipsum, ac vulputate quam. Nulla consectetur, massa sed rutrum dapibus, 
+		risus diam porttitor ipsum, sed efficitur sapien magna vitae tellus.",
+		main_image:"https://via.placeholder.com/600x400",
+		thumb_image:"https://via.placeholder.com/300x200"
+	)
+end
