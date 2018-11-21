a4aa73ef9af72cb016822069f475da89607122fc
Jonatas da Luz <jonatas.luz.91@gmail.com>
2018-11-21 16:31:36 -0200

    Adicionado modulo para rodape

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
+
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = JonatasViewTool::Renderer.copyright 'Jonatas da Luz','Seja bem vindo/a'
+	end
 end
 
 
+module JonatasViewTool
+	class Renderer
+		def self.copyright name,msg
+			"#{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
+end
