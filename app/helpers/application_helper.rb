5077e96fc8b3e30fcc6e1f54a89199cace29d41b
Jonatas da Luz <jonatas.luz.91@gmail.com>
2018-11-22 12:26:02 -0200

    Altaradas paginas de contato e sobre

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-	def login_helper
+	def login_helper style
 	 	if current_user.last_name!="Convidado"
-     	link_to "Logout", destroy_user_session_path, method: :delete 
+     	link_to "Logout", destroy_user_session_path, method: :delete, class: style
      	else  
-     	(link_to "Registre-se", new_user_registration_path) +
-     	"<br>".html_safe +
-     	(link_to "Login", new_user_session_path)
+     	(link_to "Registre-se", new_user_registration_path, class: style) +
+     	" ".html_safe +
+     	(link_to "Login", new_user_session_path , class: style)
      	end  
 	end
 	def source_helper(layout_name)
