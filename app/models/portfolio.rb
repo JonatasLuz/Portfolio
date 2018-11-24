38c8d46e098d02ea6a752c871b0120094d53b835
Jonatas da Luz <jonatas.luz.91@gmail.com>
2018-11-24 19:26:32 -0200

    Alterado mount da foto de portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,8 +5,8 @@ class Portfolio < ApplicationRecord
 	validates_presence_of :title,:body
 
 
-	mound_uploader :thumb_image, PortfolioUploader
-	mound_uploader :main_image, PortfolioUploader
+	mount_uploader :thumb_image, PortfolioUploader
+	mount_uploader :main_image, PortfolioUploader
 
 	def self.angular
 		where(subtitle: 'Angular')
